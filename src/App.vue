38f95d7f57388071d8758f17ae2d0224ea74fcf5
Akeju Paul <akejupaul10@gmail.com>
2021-10-04 21:42:20 +0100

    make it better

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-app-bar
+      flat
+      class="animate__animated animate__fadeIn animate__slower"
+      color="#1A1E27"
+      dark
+      
+    >
+      <div class="d-flex align-center">
+        
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/backendpapa"
+        target="_blank"
+        text
+        plain
+        icon
+      >
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+::-webkit-scrollbar {
+  display: none;
+}
+.nana{
+  /* background-color: ; */
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <div class="block animate__animated animate__fadeIn">
+     <v-container>
+        <v-row style="height:85.8vh;color:white" class="" no-gutters align="center" justify="center">
+      <v-col cols="12" xl="6" lg="6" class="animate__animated animate__fadeIn animate__slower">
+        <p style="font-weight:bold" class="text-h2 animate__animated animate__fadeInLeft">The <span style="color:#5CAB90">world's</span> most powerful exchange service</p>
+        <p style="font-size:17px;margin-right:20%">Get the most accurate market data, alerts,conversions,tools and more - all on this platform</p>
+        <v-row no-gutters>
+          <v-text-field flat class="text-caption font-weight-bold" placeholder="Email Address" dense solo></v-text-field>
+          <v-btn style="text-transform:none;" class="ml-3 text-caption" height="39" color="#5CAB90" dark large>Try now!  </v-btn>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-col>
+      <v-col cols="12" xl="6" lg="6">
+        
+    <!-- <v-row  no-gutters align="center" justify="center"> -->
+  <v-simple-table dense class="card animate__animated animate__tada"  >
+    <template v-slot:default>
+      
+      <tbody>
+        <tr
+        >
+          <td >USD<span class="font-weight-bold">USD</span></td>
+          <td >{{currency["USDUSD"]}}</td>
+        </tr>
+         <tr
+        >
+          <td>USD<span class="font-weight-bold">AUD</span></td>
+          <td >{{currency["USDAUD"]}}</td>
+        </tr>
+        <tr
+        >
+          <td>USD<span class="font-weight-bold">CAD</span></td>
+          <td >{{currency["USDCAD"]}}</td>
+        </tr>
+        <tr
+        >
+          <td>USD<span class="font-weight-bold">SDG</span></td>
+          <td >{{currency["USDSDG"]}}</td>
+        </tr>
+         <tr
+        >
+          <td>USD<span class="font-weight-bold">EUR</span></td>
+          <td >{{currency["USDEUR"]}}</td>
+        </tr>
+         <tr
+        >
+          <td>USD<span class="font-weight-bold">CHF</span></td>
+          <td >{{currency["USDCHF"]}}</td>
+        </tr>
+         <tr
+        >
+          <td>USD<span class="font-weight-bold">NZD</span></td>
+          <td >{{currency["USDNZD"]}}</td>
+        </tr>
+        <tr
+        >
+          <td class="green--text font-weight-bold">Total</td>
+          <td class="font-weight-bold" >{{((currency["USDUSD"]+currency["USDAUD"]+currency["USDCAD"]+currency["USDSDG"]+currency["USDEUR"]+currency["USDCHF"]+currency["USDNZD"])/7).toFixed(5)}}</td>
+        </tr>
+        
+      
+      </tbody>
+    </template>
+  </v-simple-table>
+    <!-- </v-row> -->
+      </v-col>
+    </v-row>
+     </v-container>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+  export default {
+    name: 'Home',
+    data(){
+      return {
+        currency:[]
+      }
+    },
+
+    components: {
+     
+    },
+    mounted(){
+      axios.get('http://api.currencylayer.com/live?access_key=a06cfee3d13cdc86331dfd956af98a15&currencies=USD,AUD,CAD,SDG,EUR,CHF,NZD&format=1').then(res=>{
+        this.currency=res.data.quotes
+      })
+    }
+  }
+</script>
+<style>
+.block{
+  background: url('../assets/stock.jpg'),#6DB3F2;
+  /* background: url('images/checked.png'), #6DB3F2; */
+  background-size: cover;
+  
+}
+.card {
+ 
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+}
+
+
+</style>
